e51f8c66467d5694b29d1ea4e01975c830960c8a
Thomas Bean <kennette21@gmail.com>
2019-07-24 22:30:04 +0200

    let it be

---
+++ spotifyWorkshop.py
@@ -0,0 +1,38 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID = 'c4ea3d3998de4b4282ad3bf37c3bd7c7'
+CLIENT_SECRET = 'd40ba4ca3ab44871af3bc6687dba0ba8'
+
+TB_USER_ID = '1219021298'
+
+VIBRATIONS_IN_AIR_PL_ID = '0Lb3KCUka4ZBKLregYUrtH'
+
+client_credentials_manager = SpotifyClientCredentials(CLIENT_ID, CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+playlists = sp.user_playlists(TB_USER_ID)
+
+vib_in_air_tracks = vib_tracks = sp.user_playlist_tracks(TB_USER_ID, playlist_id=VIBRATIONS_IN_AIR_PL_ID)
+album_ids = []
+count_singles = 0
+
+for track in vib_in_air_tracks['items']:
+    album_obj = track['track']['album']
+
+    # did this because i thought Single albums did not have genres... not a true assumption
+    if album_obj['album_type'] == u'single':
+        count_singles = count_singles + 1
+    else:
+        album_ids.append(album_obj['id'])
+
+print('count singles: ' + str(count_singles))
+
+# limit for this api req is 20 ids, so take the first 20
+albums_of_vib = sp.albums(album_ids[:20])
+
+for album in albums_of_vib['albums']:
+    if len(album['genres']) > 0:
+        print(album['genres'])
+    else:
+        print('no genres')
